885b31701d89b97ecedfe3e0e16041194f9cbeae
VaibhavDhamanage <vaibhavdhamanage>
2023-12-24 19:32:51 +0530

    first commit

---
+++ ExpanseAdd/src/App.jsx
@@ -0,0 +1,14 @@
+import Expenceitems from './component/Expenceitems'
+
+
+function App() {
+
+  return (
+    <>
+     <h1>Let's get Strated</h1>
+     <Expenceitems></Expenceitems>
+    </>
+  )
+}
+
+export default App
---
+++ ExpanseAdd/src/component/Expenceitems.jsx
@@ -0,0 +1,13 @@
+function Expenceitems(){
+
+    return (
+        <>
+         <h1>Expense Items</h1>
+         <li>Food Rs 10</li>
+         <li>Petrol Rs 100</li>
+         <li>Movies Rs 200</li>
+        </>
+    )
+   }
+
+   export default Expenceitems
