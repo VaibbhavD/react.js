e10e9c0bb9861efa0ee7357470e8fa8e058757be
VaibhavDhamanage <vaibhavdhamanage>
2024-01-03 10:00:48 +0530

    Add Buttons and Functionality

--- expanseitems/src/component/Expenseitems.js
+++ expanseitems/src/component/Expenseitems.js
@@ -4,16 +4,28 @@ import ExpenseDetails from './ExpenseDetails';
 import { useState } from 'react';
 
 const Expenseitems=(props)=>{
-  const[data,setdta]=useState(true)
+  const[data,setdata]=useState(true)
     const submithandle=()=>{
-        setdta(false)
+        setdata(false)
+    }
+
+  const[title,settitle]=useState(props.title)
+    const Updatehandle=()=>{
+        settitle("Updated")
+    }
+
+  const[expense,setexpense]=useState(props.price)
+    const ChangeExpense=()=>{
+        setexpense(100)
     }
     return ( data?
         (
         <div className="expense-item">
             <ExpenseDate date={props.date}/>
-            <ExpenseDetails title={props.title}  price={props.price} Location={props.Location}/> 
+            <ExpenseDetails title={title}  price={expense} Location={props.Location}/> 
             <button className="deletebtn" onClick={submithandle}>Delete</button>
+            <button className="btn" onClick={Updatehandle}>Update</button>
+            <button className="btn" onClick={ChangeExpense}>Next</button>
         </div>
     ):<></>)
    }
