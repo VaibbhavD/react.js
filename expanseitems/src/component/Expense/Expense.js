669703501ff90de9cd1c68169a530c53617f2562
VaibhavDhamanage <vaibhavdhamanage>
2024-01-04 08:38:14 +0530

    formatting

--- expanseitems/src/component/Expense/Expense.js
+++ expanseitems/src/component/Expense/Expense.js
@@ -1,9 +1,17 @@
 import Expenseitems from "./Expenseitems";
+import { useState } from "react";
+import ExpenceFilter  from './ExpenceFilter'
 
 function Expense(props,Location="none"){
+    const[year,setyear]=useState('2019')
+
+    const onChangefilter=(filtervalue)=>{
+        setyear(filtervalue);
+    }
 
     return (
         <>
+        <ExpenceFilter onChangefilter={onChangefilter}  selected={year}/>
         {props.item.map((data)=>(
             <Expenseitems  date={data.date}   title={data.title} price={data.price} Location={data.Location} />
            ))}
