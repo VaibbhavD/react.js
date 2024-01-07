376b04669064a6ad6196eac65ec124be9e533f2a
VaibhavDhamanage <vaibhavdhamanage>
2024-01-07 10:29:08 +0530

    New Project

---
+++ add-goal/src/Components/coursegoals/CourseInput/CourseInput.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import Button from '../../Ui/Buttons/CourseGuide/Button'
+import './CourseInput.css';
+
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+
+  const goalInputChangeHandler = event => {
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
