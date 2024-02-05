3f68d5bc7abc59059cf161a1237993eb8dfff4bf
VaibhavDhamanage <vaibhavdhamanage>
2024-02-05 12:03:53 +0530

    try and catch and chack responce code also

--- Movie app API's/movies_app_apis/src/App.js
+++ Movie app API's/movies_app_apis/src/App.js
@@ -5,23 +5,61 @@ import "./App.css";
 function App() {
   const [Movies, setMovies] = useState([]);
   const [loading, setloading] = useState(false);
+  const [error, seterror] = useState(null);
 
   async function submithandler() {
     setloading(true);
+    seterror(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
 
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
+      if (!response.ok) {
+        throw new Error("'Something went wrong ....Retrying' !");
+      }
 
+      const data = await response.json();
+      const temp = data.results.map((item) => {
+        return {
+          id: item.episode_id,
+          title: item.title,
+          openingText: item.opening_crawl,
+          releaseDate: item.release_date,
+        };
+      });
+      setMovies(temp);
+    } catch (error) {
+      seterror(error.message);
+    }
     setloading(false);
-    const temp = data.results.map((item) => {
-      return {
-        id: item.episode_id,
-        title: item.title,
-        openingText: item.opening_crawl,
-        releaseDate: item.release_date,
-      };
-    });
-    setMovies(temp);
+  }
+
+  let content = <h3>No Found Movies</h3>;
+
+  if (Movies.length > 0) {
+    content = <MoviesList movies={Movies} />;
+  }
+
+  if (error) {
+    content = <h3>{error}</h3>;
+  }
+
+  if (loading) {
+    content = (
+      <div class="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    );
   }
 
   return (
@@ -29,25 +67,7 @@ function App() {
       <section>
         <button onClick={submithandler}>Fetch Movies</button>
       </section>
-      <section>
-        {loading && (
-          <div class="lds-spinner">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
-        )}
-        <MoviesList movies={Movies} />
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
