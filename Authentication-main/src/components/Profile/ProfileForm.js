47ecde7589218a4688acfe5de7013e51cfed9410
VaibhavDhamanage <vaibhavdhamanage>
2024-02-07 12:12:12 +0530

    Change Password Functio Add using Rest API

--- Authentication-main/src/components/Profile/ProfileForm.js
+++ Authentication-main/src/components/Profile/ProfileForm.js
@@ -1,17 +1,62 @@
-import classes from './ProfileForm.module.css';
+import { useContext, useRef } from "react";
+import classes from "./ProfileForm.module.css";
+import { AuthContext } from "../../Store/Auth-Context";
 
 const ProfileForm = () => {
+  const PasswordRef = useRef();
+  const context = useContext(AuthContext);
+
+  const SubmitHandler = (e) => {
+    e.preventDefault();
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAsZH3qrDtweiZTyYzmdE34My1E-wKNW0A",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: context.token,
+          password: PasswordRef.current.value,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          alert("Password Changed Successfully !");
+        } else {
+          res.json().then((data) => {
+            let message = "Failed";
+            if (data && data.error && data.error.message) {
+              message = data.error.message;
+            }
+            alert(message);
+          });
+        }
+      })
+      .catch((err) => {
+        alert(err.mesage);
+      });
+  };
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={SubmitHandler} className={classes.form}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          ref={PasswordRef}
+          id="new-password"
+          minLength="7"
+          required
+        />
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        <button type="submit">Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
